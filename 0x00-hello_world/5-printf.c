db5db205e8f21343498f115532e5d8cc29a1bfac
EdwinKinyua <edwinkega@gmail.com>
2021-06-18 09:52:30 -0700

    C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point for the program
+ * @void: The function does not accept any parameters
+ * Return: int returned as 0
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
